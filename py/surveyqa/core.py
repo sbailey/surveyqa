c0f8c3e8db93244839a6f2db4b304adf2d8ec182
williyamshoe <william.sheu@berkeley.edu>
2019-04-22 12:43:33 -0700

    added docstring to check_offline_files, and changed offile_files directory location

--- py/surveyqa/core.py
+++ py/surveyqa/core.py
@@ -9,29 +9,44 @@ import urllib.request
 
 import surveyqa.summary
 import surveyqa.nightly
+from pathlib import PurePath
 
-def check_offline_files():
+def check_offline_files(dir):
+    '''
+    Checks if the Bokeh .js and .css files are present (so that the page works offline).
+    If they are not downloaded, they will be fetched and downloaded.
+
+    Args:
+        dir : directory of where the offline_files folder should be located.
+              If not present, an offline_files folder will be genreated.
+    '''
+    path=(PurePath(dir) / "offline_files")
     version = bokeh.__version__
+    b_js = (path / 'bokeh{version}.js'.format(version=version)).as_posix()
+    bt_js = (path / 'bokeh_tables{version}.js'.format(version=version)).as_posix()
+    b_css = (path / 'bokeh{version}.css'.format(version=version)).as_posix()
+    bt_css = (path / 'bokeh_tables{version}.css'.format(version=version)).as_posix()
     try:
-        fh = open('../py/offline_files/bokeh{version}.js'.format(version=version), 'r')
-        fh = open('../py/offline_files/bokeh_tables{version}.js'.format(version=version), 'r')
-        fh = open('../py/offline_files/bokeh{version}.css'.format(version=version), 'r')
-        fh = open('../py/offline_files/bokeh_tables{version}.css'.format(version=version), 'r')
+        fh = open(b_js, 'r')
+        fh = open(bt_js, 'r')
+        fh = open(b_css, 'r')
+        fh = open(bt_js, 'r')
+        print("Offline Bokeh files found")
     except FileNotFoundError:
-        shutil.rmtree("../py/offline_files", True)
-        os.mkdir("../py/offline_files")
+        shutil.rmtree(path, True)
+        os.mkdir(path)
 
         url_js = "https://cdn.pydata.org/bokeh/release/bokeh-{version}.min.js".format(version=version)
-        urllib.request.urlretrieve(url_js, '../py/offline_files/bokeh{version}.js'.format(version=version))
+        urllib.request.urlretrieve(url_js, b_js)
 
         url_tables_js = "https://cdn.pydata.org/bokeh/release/bokeh-tables-{version}.min.js".format(version=version)
-        urllib.request.urlretrieve(url_tables_js, '../py/offline_files/bokeh_tables{version}.js'.format(version=version))
+        urllib.request.urlretrieve(url_tables_js, bt_js)
 
         url_css = "https://cdn.pydata.org/bokeh/release/bokeh-{version}.min.css".format(version=version)
-        urllib.request.urlretrieve(url_css, '../py/offline_files/bokeh{version}.css'.format(version=version))
+        urllib.request.urlretrieve(url_css, b_css)
 
         url_tables_css = "https://cdn.pydata.org/bokeh/release/bokeh-tables-{version}.min.css".format(version=version)
-        urllib.request.urlretrieve(url_tables_css, '../py/offline_files/bokeh_tables{version}.css'.format(version=version))
+        urllib.request.urlretrieve(url_tables_css, bt_css)
 
         print("Downloaded offline Bokeh files")
 
@@ -47,7 +62,7 @@ def makeplots(exposures, tiles, outdir):
     Writes outdir/summary.html and outdir/night-*.html
     '''
 
-    check_offline_files()
+    check_offline_files(outdir)
 
     D = exposures["MJD"] - 51544.5
     LST = (168.86072948111115 + 360.98564736628623 * D) % 360
